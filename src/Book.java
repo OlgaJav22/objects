fd317454d77b9d7be3db7993b48eff2166b0d21c
OlgaJav22 <olga.ko4erga@yandex.ru>
2022-10-27 01:25:25 +0300

    выполнено!

--- src/Book.java
+++ src/Book.java
@@ -1,15 +1,20 @@
 public class Book {
      private String nameBook;
     private int ageBook;
-    public  Book (String nameBook, int ageBook) {
+    private Author author;
+    public  Book (String nameBook, Author author, int ageBook) {
         this.nameBook = nameBook;
         this.ageBook = ageBook;
+        this.author = author;
     }
     public String getNameBook() {
         return this.nameBook;
     }
     public int getAgeBook() {
         return this.ageBook;
+    }
+    public Author getAuthor() {
+        return this.author;
     }
 
     public void setAgeBook(int ageBook) {
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        Book story = new Book("Снежная королева", 2000);
         Author story1 = new Author("Андерсен", "Ханс Кристиан");
+        Book story = new Book("Снежная королева", story1,2000);
+
         System.out.println("Название книни; автор и год выпуска: " + story.getNameBook() + "; " + story1.getFirstNameAuthor() + " " + story1.getLastNameAuthor() + "; " + story.getAgeBook());
         story.setAgeBook(2002);
         System.out.println("Новая дата выпуска = " + story.getAgeBook());
+
+        Author lyrics = new Author("Токмакова", "Ирина");
+        Book lyrics1 = new Book("Поиграем!Стихи", lyrics, 1995);
+
+        System.out.println("Название книги; автор и год выпуска: " + lyrics1.getNameBook() + "; " + lyrics.getFirstNameAuthor() + "; " + lyrics.getLastNameAuthor() + " " +lyrics1.getAgeBook());
+
     }
 }
